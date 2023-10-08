1c22bd7dcb53167c598b992e8449fdda9cd29e13
ogzhanolguncu <ogzhan11@gmail.com>
2023-10-08 10:39:51 +0300

    Add middleware to track incoming rate limiting

--- index.ts
+++ index.ts
@@ -1,5 +1,24 @@
-export type Hello = {
-  name: "String";
-};
-export let name: Hello = { name: "String" };
-console.log("name", name);
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
+
+const app = new Hono();
+app.get("/", (c) => c.text("PONG"));
+
+//Middlewares
+app.use("*", prettyJSON());
+
+//Listen limited requests to rate limit
+app.use("/limited/:id", async (_, next) => {
+  console.log("middleware 1 start");
+  await next();
+  console.log("middleware 1 end");
+});
+
+app.get("/unlimited/:id", (c) => {
+  return c.text("Unlimited! Let's Go!");
+});
+app.get("/limited/:id", (c) => {
+  return c.text("Limited, don't over use me!");
+});
+
+export default app;
